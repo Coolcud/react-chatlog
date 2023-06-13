4a029532b77f150e36a6de2af64a3075d9ec55c2
Sage Choi <coolcuds@gmail.com>
2023-06-13 12:11:54 -0700

    Imported ChatEntry and rendered one chat entry

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
+        <ChatEntry entries={chatMessages}/>
         {/* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
