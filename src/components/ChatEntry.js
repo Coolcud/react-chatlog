82d7ad0b55bcec9f93d0dca93d927a463d641d5c
Sage Choi <coolcuds@gmail.com>
2023-06-13 12:55:39 -0700

    Changed spacing and used props for retrieving values

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -4,22 +4,22 @@ import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
-  return (
-    <div className="chat-entry local">
-      <h2 className="entry-name">{props.entries[0]['sender']}</h2>
-      <section className="entry-bubble">
-        <p>{props.entries[0]['body']}</p>
-        <p className="entry-time"><TimeStamp time={props.entries[0]['timeStamp']}/></p>
-        <button className="like">🤍</button>
-      </section>
-    </div>
-  );
+    return (
+        <div className="chat-entry local">
+            <h2 className="entry-name">{props.name}</h2>
+            <section className="entry-bubble">
+                <p>{props.body}</p>
+                <p className="entry-time"><TimeStamp time={props.time}/></p>
+                <button className="like">🤍</button>
+            </section>
+        </div>
+    );
 };
 
 ChatEntry.propTypes = {
-  senderName: PropTypes.string.isRequired,
-  msgBody: PropTypes.string.isRequired,
-  msgTime: PropTypes.string.isRequired
+    name: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired
 };
 
 export default ChatEntry;
