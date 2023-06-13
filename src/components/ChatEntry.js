6b54eb65bb1822ee806090d30c8e096a476aa8c2
Sage Choi <coolcuds@gmail.com>
2023-06-13 12:19:14 -0700

    Added proptypes and name, message body, and timestamp for first chat message

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './ChatEntry.css';
 import PropTypes from 'prop-types';
+import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
   return (
     <div className="chat-entry local">
-      <h2 className="entry-name">Replace with name of sender</h2>
+      <h2 className="entry-name">{props.entries[0]['sender']}</h2>
       <section className="entry-bubble">
-        <p>Replace with body of ChatEntry</p>
-        <p className="entry-time">Replace with TimeStamp component</p>
+        <p>{props.entries[0]['body']}</p>
+        <p className="entry-time"><TimeStamp time={props.entries[0]['timeStamp']}/></p>
         <button className="like">🤍</button>
       </section>
     </div>
@@ -16,7 +17,9 @@ const ChatEntry = (props) => {
 };
 
 ChatEntry.propTypes = {
-  //Fill with correct proptypes
+  senderName: PropTypes.string.isRequired,
+  msgBody: PropTypes.string.isRequired,
+  msgTime: PropTypes.string.isRequired
 };
 
 export default ChatEntry;
