d18624ff30bc0746bd9311cf66f150abd9d0e2c4
Sage Choi <coolcuds@gmail.com>
2023-06-14 15:57:27 -0700

    Added heart counter functionality and onUpdateEntry prop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
 
 const App = () => {
     const localUser = chatMessages[0]['sender'];
+    const [entryData, setEntryData] = useState(chatMessages);
+
+    const updateEntryData = updatedEntry => {
+        const entries = entryData.map(entry => {
+            if (entry.id === updatedEntry.id) {
+                return updatedEntry;
+            } else {
+                return entry;
+            }
+        });
+
+        setEntryData(entries);
+    };
+
+    const totalHearts = entryData.filter(e => e.liked).length;
 
     return (
         <div id="App">
             <header>
                 <h1>Chat between Vladimir and Estragon</h1>
-                <section>{"❤️'s"}</section>
+                <section>
+                    <h2 id='heartWidget' className='widget'>{totalHearts} ❤️s</h2>
+                </section>
             </header>
             <main>
-                <ChatLog entries={chatMessages} local={localUser}/>
+                <ChatLog
+                    entries={entryData}
+                    local={localUser}
+                    onUpdateEntry={updateEntryData}
+                />
             </main>
         </div>
     );
