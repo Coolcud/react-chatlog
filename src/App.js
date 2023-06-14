05f883905332b01629ab5091c78dd2223e1b7aa3
Sage Choi <coolcuds@gmail.com>
2023-06-14 13:40:29 -0700

    Determined local user for formatting messages for different users

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
 
 const App = () => {
+    const localUser = chatMessages[0]['sender'];
+
     return (
         <div id="App">
             <header>
                 <h1>Chat between Vladimir and Estragon</h1>
+                <section>{"❤️'s"}</section>
             </header>
             <main>
-                <ChatLog entries={chatMessages}/>
+                <ChatLog entries={chatMessages} local={localUser}/>
             </main>
         </div>
     );
