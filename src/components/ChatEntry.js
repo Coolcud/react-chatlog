c00d4c2fffac56c3c175723813c5289463086b49
Sage Choi <coolcuds@gmail.com>
2023-06-13 22:46:39 -0700

    Added conditional to add styling for each different user

--- src/components/ChatEntry.js
+++ src/components/ChatEntry.js
@@ -4,12 +4,20 @@ import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
 const ChatEntry = (props) => {
+    const formatMsgs = () => {
+        if (props.sender === props.senders[0]) {
+            return 'chat-entry local'
+        } else {
+            return 'chat-entry remote'
+        }
+    };
+
     return (
-        <div className="chat-entry local">
-            <h2 className="entry-name">{props.name}</h2>
+        <div className={formatMsgs()}>
+            <h2 className="entry-name">{props.sender}</h2>
             <section className="entry-bubble">
                 <p>{props.body}</p>
-                <p className="entry-time"><TimeStamp time={props.time}/></p>
+                <p className="entry-time"><TimeStamp time={props.timeStamp}/></p>
                 <button className="like">🤍</button>
             </section>
         </div>
@@ -17,9 +25,11 @@ const ChatEntry = (props) => {
 };
 
 ChatEntry.propTypes = {
-    name: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    sender: PropTypes.string.isRequired,
     body: PropTypes.string.isRequired,
-    time: PropTypes.string.isRequired
+    timeStamp: PropTypes.string.isRequired,
+    liked: PropTypes.bool.isRequired
 };
 
 export default ChatEntry;
