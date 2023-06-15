aa1aeec6b06f1c5baebd5328dd3f7ffca0271b5c
Sage Choi <coolcuds@gmail.com>
2023-06-14 23:58:56 -0700

    Added react hooks for setting local and remote user colors

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import './App.css';
 import chatMessages from './data/messages.json';
 import ChatLog from './components/ChatLog';
+import ColorChoice from './components/ColorChoice';
 
 const App = () => {
-    const localUser = chatMessages[0]['sender'];
     const [entryData, setEntryData] = useState(chatMessages);
-
     const updateEntryData = updatedEntry => {
         const entries = entryData.map(entry => {
             if (entry.id === updatedEntry.id) {
@@ -19,21 +18,45 @@ const App = () => {
         setEntryData(entries);
     };
 
+    const localUser = chatMessages[0]['sender'];
+    let remoteUser;
+    for (let entry of entryData) {
+        if (entry.sender !== localUser) {
+            remoteUser = entry.sender;
+            break;
+        }
+    }
+
+    const [localColor, setLocalColor] = useState('black');
+    const [remoteColor, setRemoteColor] = useState('black');
+
     const totalHearts = entryData.filter(e => e.liked).length;
 
     return (
-        <div id="App">
+        <div id='App'>
             <header>
-                <h1>Chat between Vladimir and Estragon</h1>
+                <h1>Chat between {localUser} and {remoteUser}</h1>
                 <section>
+                    <ColorChoice
+                        setColorCallback={newColor => setLocalColor(newColor)}
+                        sender={localUser}
+                        color={localColor}
+                    />
                     <h2 id='heartWidget' className='widget'>{totalHearts} ❤️s</h2>
+                    <ColorChoice
+                        setColorCallback={newColor => setRemoteColor(newColor)}
+                        sender={remoteUser}
+                        color={remoteColor}
+                    />
                 </section>
             </header>
             <main>
                 <ChatLog
                     entries={entryData}
-                    local={localUser}
                     onUpdateEntry={updateEntryData}
+                    local={localUser}
+                    localColor={localColor}
+                    remoteColor={remoteColor}
                 />
             </main>
         </div>
